bd1aecc57196eef737abdd812f47a673d971fc24
Gero Gerke <hello@gero.dev>
2024-08-28 01:24:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+<div align="center">
+    <br/>
+    <h1>mqtt2postgres</h1>
+    <strong>This project aims to implement a MQTT Message Broker to Postgres interface. It is currently not advised to use this project for anything, as the current code serves more as a MVP than anything else.</strong>
+</div>
+<br/>
+<p align="center">
+    <a href="https://www.rust-lang.org/en-US/">
+        <img src="https://img.shields.io/badge/Made%20with-Rust-orange.svg" alt='Build with Rust' />
+    </a>
+    <img src="https://img.shields.io/badge/rustc-1.82+-yellow.svg" alt='Minimum Rust Version: 1.82.0-nightly' />
+    <img src="https://img.shields.io/badge/Postgres-9.1+-green.svg" alt='Minimum Postgres Version: 9.1' />
+</p>
+
+Pull requests are always welcome. For a list of past changes, see [CHANGELOG.md][__link2].
+
+
+### Currently Supported Features
+
+ - Table generation and insert based on zigbee2mqtt JSON schema
+
+
+## Quickstart
+
+You will need to fork the project and edit the code at this time. There are no configuration variables available, everything is hardcoded.
+
+This project has only been tested with a zigbee2mqtt server. It's not garantueed to work with anything else.
+
+## License
+
+[![License: MIT][__link16]][__link17]
+
+@ 2024 Gero Gerke and [contributors].
+
+ [contributors]: https://github.com/influxdb-rs/influxdb-rust/graphs/contributors
+ [__link16]: https://img.shields.io/badge/License-MIT-yellow.svg
+ [__link17]: https://opensource.org/licenses/MIT
---
+++ src/main.rs
@@ -0,0 +1,154 @@
+#![feature(ascii_char)]
+
+use rumqttc::v5::mqttbytes::QoS;
+use tokio::{task};
+
+use slugify::slugify;
+
+use rumqttc::v5::{AsyncClient, MqttOptions};
+use std::time::Duration;
+
+use rumqttc::v5::mqttbytes::v5::Packet;
+use rumqttc::v5::Event;
+
+use bytes::Bytes;
+
+use tokio_postgres::{NoTls};
+
+use anyhow::{anyhow, Context};
+use serde_json::Value;
+use std::collections::HashMap;
+
+use serde_json::Value::Number;
+use serde_json::Value::Bool;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> anyhow::Result<()> {
+    pretty_env_logger::init();
+    // color_backtrace::install();
+
+    let mut mqttoptions = MqttOptions::new("test-1", "localhost", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
+    mqttoptions.set_max_packet_size(Some(100000));
+
+    let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+    task::spawn(async move {
+        requests(client).await;
+    });
+
+    let (client, connection) =
+    tokio_postgres::connect("postgresql://postgres:postgres@localhost:5432/mqtt2postgres", NoTls).await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    loop {
+        let event = eventloop.poll().await;
+        match &event {
+            Ok(v) => match v {
+                Event::Incoming(Packet::Publish(publish)) => {
+                    let topic = slugify_topic(&publish.topic);
+
+                    if topic.len() >= 2 && topic[1] != "bridge" && !publish.dup {
+                        println!();
+                        let table_name = topic.join("_"); // FIXME: turn back to . and make use of postgres schemata
+                        // println!("Properties = {:?}", publish.properties);
+
+                        let schema = build_schema_from_payload(&table_name, &publish.payload);
+
+                        // let rows = client
+                        //     .query("SELECT $1::TEXT", &[&"hello world"])
+                        //     .await?;
+
+                        match schema {
+                            Ok(schema) => {
+                                // TODO: keep a copy of them schema in RAM; if it is unchanged, do not submit this query
+                                let table = client.query(&schema, &[]).await?;
+                                
+                                let insert = build_insert_query(&table_name, &publish.payload);
+                                println!("{}", &insert.as_ref().unwrap());
+                                let insertresult = client.query(&insert.unwrap(), &[]).await?;
+                            }
+                            Err(e) => println!("Error: {:?}", e)
+                        }
+                    }
+                }
+                _ => {}
+            },
+            Err(e) => {
+                println!("Error = {e:?}");
+                return Ok(());
+            }
+        }
+    }
+}
+
+fn build_schema_from_payload(table_name: &String, payload: &Bytes) -> anyhow::Result<String> {
+    let m: HashMap<String, Value> = serde_json::from_slice(&payload)?;
+
+    let mut fields = Vec::with_capacity(m.len());
+
+    for (k, v) in m {
+        let datatype = extract_datatype(&v);
+
+        // TODO: refactor to use Some/None and to support nested objects
+        if datatype != "other" {
+            fields.push(format!("{} {}", k, datatype));
+        }
+    }
+
+    return Ok(format!("CREATE TABLE IF NOT EXISTS {} (timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, {});", table_name, fields.join(", ")));
+}
+
+fn build_insert_query(table_name: &String, payload: &Bytes) -> anyhow::Result<String> {
+    let m: HashMap<String, Value> = serde_json::from_slice(&payload)?;
+
+    let mut keys = Vec::with_capacity(m.len());
+    let mut values = Vec::with_capacity(m.len());
+
+    for (k, v) in m {
+        let datatype = extract_datatype(&v);
+
+        // TODO: refactor to use Some/None and to support nested objects
+        if datatype != "other" {
+            keys.push(k);
+            if v.is_string() {
+                values.push(format!(r#"'{}'"#, v.as_str().unwrap()));
+            } else {
+                values.push(v.to_string());
+            }
+        }
+    }
+
+    return Ok(format!("INSERT INTO {} ({}) VALUES ({});", table_name, keys.join(", "), values.join(", ")));
+}
+
+
+fn extract_datatype(value: &Value) -> &str {
+    match value {
+        Number(_) => "numeric",
+        Bool(_) => "boolean",
+        serde_json::Value::String(_) => "text",
+        // TODO: how to handle this properly?
+        serde_json::Value::Null => "text",
+        _ => "other"
+    }
+}
+
+fn slugify_topic(topic: &Bytes) -> Vec<String> {
+    let parts = topic.escape_ascii().to_string();
+    parts
+        .split("/")
+        .map(|part| slugify!(part, separator = "_"))
+        .collect::<Vec<String>>()
+}
+
+async fn requests(client: AsyncClient) {
+    client
+        .subscribe("zigbee2mqtt/#", QoS::AtMostOnce)
+        .await
+        .unwrap();
+}
